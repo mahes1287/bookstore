93ba72ff4dc7dbec706aeae901c969712f688ae4
mahes1287 <mahes1287@gmail.com>
2023-08-13 01:17:03 +0200

    conditional rendering of DEBUG

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d=%21euq@=ye$7n7p!m9pqrzqakjr#x8cjx)ls95cx+hoa(*it'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "True") == True
 
 ALLOWED_HOSTS = []
 
